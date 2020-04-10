9d9ef1e37ba6a5ccf3a95fdebe770011a59099b2
afrye1997 <allisonfrye97@gmail.com>
2020-04-10 15:06:54 -0500

    idk

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-//  "use strict"
-//  const express= require('express')
-//  const cors= require('cors')
-//  const app= express();
-//  const students= require ("./routes/students")
-//  const port=4000;
+ "use strict"
+ const express= require('express')
+ const cors= require('cors')
+ const app= express();
+ const students= require ("./routes/students")
+ const port=4000;
 
 
  
  
 
-// app.use(cors());
-//  app.use("/students", students);
-//  //use the agents file to handle  endpoints that start with /things
+app.use(cors());
+ app.use("/students", students);
+ //use the agents file to handle  endpoints that start with /things
 
 
-//  app.get("/", (req,res)=>{
-//      res.send("server is running")
-//  });
+ app.get("/", (req,res)=>{
+     res.send("server is running")
+ });
 
-// app.listen(port,  err=>{
-//     if (err)
-//         return  console.log("ERROR", err);
-//     else
-//         console.log(`listening on port ${port}`)
-// });
+app.listen(port,  err=>{
+    if (err)
+        return  console.log("ERROR", err);
+    else
+        console.log(`listening on port ${port}`)
+});
 
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
-
-var port = 4000
-app.listen(port)
-console.log('server started ' + port)
