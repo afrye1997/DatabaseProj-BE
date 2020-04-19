c74383535d0b4cf4612ab75a5250aae9d115d2f2
afrye1997 <allisonfrye97@gmail.com>
2020-04-19 15:45:32 -0500

    idk

--- routes/enrollment.js
+++ routes/enrollment.js
@@ -1,40 +1,39 @@
 "use  strict";
-const express= require("express");
+const express = require("express");
 const router = express.Router();
-var connection = require('../connection');
+var connection = require("../connection");
 
-router.route("/getEnrollment")
-.get(async (req, res, next)=> {
-    connection.query('SELECT * FROM ENROLLMENT', function (error, results, fields) {
-        if (error) throw error;
-        console.log('The solution is: ', results);
-        res.send(results);
-      });
+router.route("/getEnrollment").get(async (req, res, next) => {
+  connection.query("SELECT * FROM ENROLLMENT", function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    console.log("The solution is: ", results);
+    res.send(results);
+  });
+});
 
-   
-}) 
+router.route("/addEnrollment").get(async (req, res, next) => {
+  //  students/addStudents
+  const {
+    ENROLLMENT_ID,
+    STUDENT_ID,
+    COURSE_DEPTCODE,
+    COURSE_COURSENUM,
+  } = req.query;
 
-
-router.route("/addEnrollment")
-.get(async (req, res, next)=> {
-    //  students/addStudents 
-    const {ENROLLMENT_ID, STUDENT_ID, COURSE_DEPTCODE, COURSE_COURSENUM}= req.query;
-    
-    const INSERT_ENROLLMENT_QUERY= `INSERT INTO  ENROLLMENT VALUES (${ENROLLMENT_ID}, ${STUDENT_ID}, '${COURSE_DEPTCODE}', ${COURSE_COURSENUM})`;
-    connection.query(INSERT_ENROLLMENT_QUERY, function (error, results) {
-        if (error)
-        {
-          return res.send(error.code)
-        }
-        else{
-          console.log(results)
-          var result ="Applicant was added successfully!";
-            return res.send(result)
-        }
-      });
-   
-}) 
-
-
+  const INSERT_ENROLLMENT_QUERY = `INSERT INTO  ENROLLMENT VALUES (${ENROLLMENT_ID}, ${STUDENT_ID}, '${COURSE_DEPTCODE}', ${COURSE_COURSENUM})`;
+  connection.query(INSERT_ENROLLMENT_QUERY, function (error, results) {
+    if (error) {
+      return res.send(error.code);
+    } else {
+      console.log(results);
+      var result = "Applicant was added successfully!";
+      return res.send(result);
+    }
+  });
+});
 
 module.exports = router;
